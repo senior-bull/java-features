b488a47ef3ccec4f418e95173aa8d85fd9186370
ivanov1024 <14792518+ivanov1024@users.noreply.github.com>
2020-01-09 21:53:26 +0400

    type deduce lambda

--- src/org/test/java11/var/TypeDeduceForLambda.java
+++ src/org/test/java11/var/TypeDeduceForLambda.java
@@ -1,2 +1,32 @@
-package org.test.java11.var;public class TypeDeduceForLambda {
+package org.test.java11.var;
+
+import java.util.function.Consumer;
+
+public class TypeDeduceForLambda {
+
+    public @interface TestAnnotation {
+
+    }
+
+    @FunctionalInterface
+    public interface TestConsumer<T> {
+
+        void accept(@TestAnnotation T x);
+    }
+
+    public static void main(String[] args) {
+
+        Consumer<String> c = (var x) -> {
+            System.out.println(x);
+        };
+
+        TestConsumer<String> z = (var x) -> {
+            System.out.println(x);
+        };
+
+        // NOT possible with annotations
+        TestConsumer<String> z1 = (@TestAnnotation var x) -> {
+            System.out.println(x);
+        };
+    }
 }

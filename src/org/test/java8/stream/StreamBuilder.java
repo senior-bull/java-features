ab25b54b36f27647d4f755d20ca5ef48dc71c1a8
ivanov1024 <14792518+ivanov1024@users.noreply.github.com>
2020-01-11 19:43:19 +0400

    fix

--- src/org/test/java8/stream/StreamBuilder.java
+++ src/org/test/java8/stream/StreamBuilder.java
@@ -1,2 +1,11 @@
-package org.test.java8.stream;public class StreamBuilder {
+package org.test.java8.stream;
+
+import java.util.stream.Stream;
+
+public class StreamBuilder {
+
+    public static void main(String[] args) {
+        Stream.builder().add("a").add("b").add("c").build()
+                .forEach(System.out::println);
+    }
 }

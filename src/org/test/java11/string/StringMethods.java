9ba9d892563801cb661209b1313c12e2643e2241
ivanov1024 <14792518+ivanov1024@users.noreply.github.com>
2020-12-29 21:53:26 +0400

    Repeat method test

--- src/org/test/java11/string/StringMethods.java
+++ src/org/test/java11/string/StringMethods.java
@@ -6,5 +6,7 @@ public class StringMethods {
 
     public static void main(String[] args) {
         System.out.println("asdasdsa\nzxczxczc\nascsad".lines().collect(Collectors.joining()));
+
+        System.out.println("ABC".repeat(5));
     }
 }

9014b536705f998bee476234ba09e1fa56ac5246
ivanov1024 <14792518+ivanov1024@users.noreply.github.com>
2021-01-03 21:53:26 +0400

    Example of using java thread

--- src/org/test/java16/loom/StartVirtualThread.java
+++ src/org/test/java16/loom/StartVirtualThread.java
@@ -1,2 +1,15 @@
-package org.test.java16.loom;public class StartVirtualThread {
+package org.test.java16.loom;
+
+public class StartVirtualThread {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = Thread.startVirtualThread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+
+        thread.join();
+    }
 }

94b1cfe0ed8fd04969a0c366b3bae99a25d3aa89
ivan4096 <14792518+ivan4096@users.noreply.github.com>
2021-02-08 20:30:26 +0400

    Files.mismatch test

--- src/org/test/java12/files/FilesMismatch.java
+++ src/org/test/java12/files/FilesMismatch.java
@@ -1,4 +1,19 @@
 package org.test.java12.files;
 
-public class Files {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FilesMismatch {
+
+    public static void main(String[] args) throws IOException {
+        Path file1 = Files.createTempFile("a", "b");
+        Path file2 = Files.createTempFile("a", "b");
+
+        Files.writeString(file1, "abcdef");
+        Files.writeString(file2, "abxdef");
+
+        // 2
+        System.out.println(Files.mismatch(file1, file2));
+    }
 }

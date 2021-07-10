8981dc86c749379b40bcfe613507074fb3945415
Vitaliy Bull <14792518+senior-bull@users.noreply.github.com>
2021-07-10 15:01:05 +0400

    Helpful NPE test

---
+++ src/org/test/java15/HelpfulNPETest.java
@@ -0,0 +1,28 @@
+package org.test.java15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelpfulNPETest {
+
+    private static class Node {
+
+        private final Node next;
+
+        private Node(Node next) {
+            this.next = next;
+        }
+
+        public Node next() {
+            return next;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Node> list = new ArrayList<>();
+        list.add(new Node(null));
+        list.add(null);
+
+        System.out.println(list.get(1).next());
+    }
+}
